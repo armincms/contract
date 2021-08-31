e7798a2230d985d94e66fbbfe19b15ee805e33b8
اسماعیل زارع <zarehesmaiel@gmail.com>
2021-08-31 17:38:20 +0430

    Added RTL theme

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -24,5 +24,6 @@ class ServiceProvider extends AggregateServiceProvider
     public function boot()
     {    
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        app('config')->set('app.locale', 'fa');
     }
 }
