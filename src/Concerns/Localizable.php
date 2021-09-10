d2d6021e625db89b09f3f975a493b1b8f84e2277
اسماعیل زارع <zarehesmaiel@gmail.com>
2021-09-10 11:36:01 +0430

    Added Localization cncerns

---
+++ src/Concerns/Localizable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Armincms\Contract\Concerns;
+
+use Armincms\Contract\Casts\Url;
+ 
+trait Localizable  
+{    
+    /**
+     * Query where has the app locale.
+     * 
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder       
+     */
+    public function scopeLocalize($query)
+    {
+        return $query->hasLocale((array) app()->getLocale());
+    }
+
+    /**
+     * Query where has the given locales.
+     * 
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder       
+     */
+    public function scopeHasLocale($query, array $locales)
+    {
+        return $query->whereIn($this->getQualifiedLocaleName(), (array) app()->getLocale());
+    }
+ 
+    /**
+     * Get the table qualified locale name.
+     *
+     * @return string
+     */
+    public function getQualifiedLocaleName()
+    {
+        return $this->qualifyColumn($this->getLocaleName());
+    }
+
+    /**
+     * Get the locale for the model.
+     *
+     * @return string
+     */
+    public function getLocaleName()
+    {
+        return 'locale';
+    }
+}
