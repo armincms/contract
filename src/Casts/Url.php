dfd5b4b120a186414eb86e48e3a01243341c57bb
اسماعیل زارع <zarehesmaiel@gmail.com>
2021-09-10 07:54:59 +0430

    missed implemented HasUri

---
+++ src/Casts/Url.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Armincms\Contract\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class Url implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return array
+     */
+    public function get($model, $key, $value, $attributes)
+    {
+        return urldecode($value);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  array  $value
+     * @param  array  $attributes
+     * @return string
+     */
+    public function set($model, $key, $value, $attributes)
+    {
+        return urlencode($value);
+    }
+}
