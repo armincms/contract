c8315c5c2c82e070e3bdbfce21b808afbfdaa8d0
اسماعیل زارع <zarehesmaiel@gmail.com>
2021-09-15 19:01:37 +0430

    Create HasProfile.php

---
+++ src/Models/HasProfile.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Armincms\Contract\Models;
+
+use Armincms\Papyrus\Models\PapyrusPage as Model;
+
+trait HasProfile 
+{ 
+    /**
+     * Get the fullname.
+     * 
+     * @return 
+     */
+    public function fullname()
+    {
+        return $this->firstname() .PHP_EOL. $this->lastname();
+    }
+
+    /**
+     * Get the firstname.
+     * 
+     * @return 
+     */
+    public function firstname()
+    {
+        return optional($this->profile)->get('firstname');
+    }
+
+    /**
+     * Get the lastname.
+     * 
+     * @return 
+     */
+    public function lastname()
+    {
+        return optional($this->profile)->get('lastname');
+    }
+}
