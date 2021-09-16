e054cc6d2eba091652b2bd30d8960713a128e3db
اسماعیل زارع <zarehesmaiel@gmail.com>
2021-09-16 17:25:57 +0430

    Added base policy

---
+++ src/Policies/Policy.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Armincms\Contract\Policies;
+ 
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Contracts\Auth\Authenticatable;  
+use Illuminate\Database\Eloquent\Model;  
+
+abstract class Policy
+{
+    use HandlesAuthorization; 
+    
+    /**
+     * Determine whether the user can view the admin.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  \Illuminate\Database\Eloquent\Model  $admin
+     * @return mixed
+     */
+    public function view(Authenticatable $user, Model $model)
+    { 
+    }
+
+    /**
+     * Determine whether the user can create admins.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @return mixed
+     */
+    public function create(Authenticatable $user)
+    { 
+    }
+
+    /**
+     * Determine whether the user can update the admin.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  \Illuminate\Database\Eloquent\Model  $admin
+     * @return mixed
+     */
+    public function update(Authenticatable $user, Model $model)
+    { 
+    }
+
+    /**
+     * Determine whether the user can delete the admin.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  \Illuminate\Database\Eloquent\Model  $admin
+     * @return mixed
+     */
+    public function delete(Authenticatable $user, Model $model)
+    { 
+    }
+}
---
+++ src/Policies/SoftDeletes.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Armincms\Contract\Policies;  
+
+use Illuminate\Contracts\Auth\Authenticatable;  
+use Illuminate\Database\Eloquent\Model;  
+
+trait SoftDeletes
+{ 
+
+    /**
+     * Determine whether the user can restore the admin.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  \Illuminate\Database\Eloquent\Model  $admin
+     * @return mixed
+     */
+    public function restore(Authenticatable $user, Model $model)
+    { 
+    }
+
+    /**
+     * Determine whether the user can permanently delete the admin.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  \Illuminate\Database\Eloquent\Model  $admin
+     * @return mixed
+     */
+    public function forceDelete(Authenticatable $user, Model $model)
+    { 
+    }
+}
