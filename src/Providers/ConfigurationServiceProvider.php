abc2a5a96162aff5419b75015968be106795050d
اسماعیل زارع <zarehesmaiel@gmail.com>
2021-09-21 13:04:27 +0430

    Configure menu builder

--- src/Providers/ConfigurationServiceProvider.php
+++ src/Providers/ConfigurationServiceProvider.php
@@ -3,7 +3,10 @@
 namespace Armincms\Contract\Providers;
      
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;  
-use Illuminate\Support\Str;  
+use Illuminate\Support\Str; 
+use OptimistDigital\MenuBuilder\MenuItemTypes\BaseMenuItemType; 
+use ReflectionClass; 
+use Symfony\Component\Finder\Finder; 
 
 class ConfigurationServiceProvider extends LaravelServiceProvider
 { 
@@ -13,11 +16,58 @@ class ConfigurationServiceProvider extends LaravelServiceProvider
      * @return void
      */
     public function boot()
-    {  
+    {    
         app('config')->set('app.locale', 'fa');
         app('config')->set('sluggable.source', 'name');
         app('config')->set('sluggable.method', [Str::class, 'sluggable']);
+        $this->menus();
+        
+        if (! $this->app->runningInConsole()) {
+            $this->setUncacheableConfigurations(); 
+        }
     }  
+
+    /**
+     * Set configurations that could not be cached.
+     * 
+     * @return void
+     */
+    public function setUncacheableConfigurations()
+    {
+        app('config')->set('nova-menu.locales', function() {
+            return (array) app("nova-targomaan.locales");
+        });   
+    }
+
+    /**
+     * Register the application's menus.
+     *
+     * @return void
+     */
+    protected function menus()
+    { 
+        $this->mergeConfigFrom(dirname(dirname(dirname(__FILE__))).'/config/nova-menu.php', 'nova-menu');
+
+        $namespace = 'Armincms\\Contract\\Menus\\';
+        $directory = dirname(__DIR__).DIRECTORY_SEPARATOR.'Menus'; 
+
+        $menus = (array) config('nova-menu.menu_item_types');
+
+        foreach ((new Finder)->in($directory)->files() as $menu) {
+            $menu = $namespace.str_replace(
+                ['/', '.php'],
+                ['\\', ''],
+                Str::after($menu->getPathname(), $directory.DIRECTORY_SEPARATOR)
+            );  
+
+            if (is_subclass_of($menu, BaseMenuItemType::class) &&
+                ! (new ReflectionClass($menu))->isAbstract()) {
+                $menus[] = $menu;
+            }
+        }
+
+        app('config')->set('nova-menu.menu_item_types', $menus);
+    }
 
     /**
      * Register any application services.
