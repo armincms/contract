11aeed9a39b379b98e433294fdc934a89625d0e5
Ismail Zare <zarehesmaiel@gmail.com>
2022-01-24 22:22:13 +0330

    Added resource localization trait

---
+++ src/Nova/Localization.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Armincms\Contract\Nova;
+
+use Illuminate\Support\Str;
+
+trait Localization  
+{    
+    /**
+     * Get the displayable label of the resource.
+     *
+     * @return string
+     */
+    public static function label()
+    {
+        return __(parent::label());
+    }
+
+    /**
+     * Get the displayable singular label of the resource.
+     *
+     * @return string
+     */
+    public static function singularLabel()
+    {
+        return __(Str::singular(parent::label()));
+    }
+}
