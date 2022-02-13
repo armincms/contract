14039c82057dfb8141bea3ecc0733f911d66522b
Ismail Zare <zarehesmaiel@gmail.com>
2022-02-13 11:52:41 +0330

    Added helper for widget to work with templates

---
+++ src/Gutenberg/Widgets/BootstrapsTemplate.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Armincms\Contract\Gutenberg\Widgets; 
+
+use Zareismail\Gutenberg\Gutenberg;
+
+trait BootstrapsTemplate
+{     
+    /**
+     * Bootstrap template for the given id.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Zareismail\Gutenberg\GutenbergLayout $layout
+     * @param  integer  $templateKey
+     * @return \Zareismail\Gutenberg\GutenbergTemplate
+     */
+    public function bootstrapTemplate($request, $layout, $templateKey)
+    {   
+        return tap($this->resolveTemplate($templateKey), function($template) use ($request, $layout) { 
+            $template->plugins
+                ->filter->isActive()
+                ->flatMap->gutenbergPlugins()
+                ->each->boot($request, $layout);
+        }); 
+    }
+
+    /**
+     * resolve template instance for the given id.
+     * 
+     * @param  integer $templateKey
+     * @return $this
+     */
+    public function resolveTemplate($templateKey)
+    {  
+        return tap(Gutenberg::cachedTemplates()->find($templateKey), function($template) {
+            abort_if(
+                is_null($template),
+                422, 
+                "Template not found to display widget: ". class_basename($this),
+            ); 
+        });
+    }
+}
---
+++ src/Gutenberg/Widgets/ResolvesDisplay.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Armincms\Contract\Gutenberg\Widgets; 
+
+trait ResolvesDisplay
+{    
+    /**
+     * The callback to be used to resolve the resourc's display.
+     *
+     * @var \Closure
+     */
+    public $displayResourceCallback = []; 
+
+    /**
+     * Define the callback that should be used to display the resources.
+     *
+     * @param  callable $displayResourceCallback
+     * @param  string   $resource
+     * @return $this
+     */
+    public function displayResourceUsing(callable $displayResourceCallback, string $resource = null)
+    {
+        $this->displayResourceCallback[$resource] = $displayResourceCallback;
+
+        return $this;
+    }
+
+    /**
+     * Display resource for the given attributes.
+     * 
+     * @param  array $attributes 
+     * @param  string $resource   
+     * @return string             
+     */
+    public function displayResource(array $attributes, string $resource = null)
+    { 
+        return isset($this->displayResourceCallback[$resource])
+            ? call_user_func($this->displayResourceCallback[$resource], $attributes) 
+            : '';
+    }  
+}
