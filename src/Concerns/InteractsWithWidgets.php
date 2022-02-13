2c58c355f6ca08138e9e29ca7ad98920cf654f2b
Ismail Zare <zarehesmaiel@gmail.com>
2022-02-13 11:50:23 +0330

    Added ability to serialize for index

--- src/Concerns/InteractsWithWidgets.php
+++ src/Concerns/InteractsWithWidgets.php
@@ -7,12 +7,36 @@ use Zareismail\Gutenberg\Models\GutenbergWidget;
 trait InteractsWithWidgets  
 {     
     /**
-     * Serialize the model for pass into the client view.
+     * Serialize the model to pass into the client view.
      *
      * @param Zareismail\Cypress\Request\CypressRequest
      * @return array
      */
-    public function serializeForWidget($request): array
+    public function serializeForWidget($request, $detail = ture): array
+    {
+        return $detail 
+            ? $this->serializeForDetailWidget($request) 
+            : $this->serializeForIndexWidget($request);
+    }
+
+    /**
+     * Serialize the model to pass into the client view for single item.
+     *
+     * @param Zareismail\Cypress\Request\CypressRequest
+     * @return array
+     */
+    public function serializeForDetailWidget($request)
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Serialize the model to pass into the client view for collection of items.
+     *
+     * @param Zareismail\Cypress\Request\CypressRequest
+     * @return array
+     */
+    public function serializeForIndexWidget($request)
     {
         return $this->toArray();
     }
