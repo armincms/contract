3e6d5f25ae70d59a1ee7301d1e280fd2316d3545
Ismail Zare <zarehesmaiel@gmail.com>
2022-06-01 22:59:54 +0430

    add some helper traits

---
+++ src/Concerns/GeneratesTrackingCode.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Armincms\Contract\Concerns;
+
+use Armincms\Contract\Casts\Url;
+ 
+trait GeneratesTrackingCode  
+{    
+    /**
+     * Handles booting model.
+     * 
+     * @return void
+     */
+    public static function bootGeneratesTrackingCode()
+    {
+        static::saving(function($model) {
+            $model->isTrackable() || $model->fillTrackingCode();
+        });
+    }
+
+    /**
+     * Determine if the model has tracking_code.
+     * 
+     * @return boolean
+     */
+    public function isTrackable(): bool
+    {
+        return ! blank($this->trackingCode());
+    }
+
+    /**
+     * Fill the tracking_code attribute.
+     * 
+     * @return $this
+     */
+    public function fillTrackingCode()
+    {
+        $this->forceFill([
+            $this->getTrackingCodeColumn() => $this->generateTrackingCode()
+        ]);
+
+        return $this;
+    } 
+
+    /**
+     * Generate unique string tracking_code.
+     * 
+     * @return string
+     */
+    public function generateTrackingCode()
+    {
+        while (static::tracking($code = $this->generateRandomCode())->whereKeyNot($this->id)->count());
+
+        return $code;
+    }   
+
+    /**
+     * Generate new random code.
+     *  
+     * @return string       
+     */
+    public function generateRandomCode(): string
+    {
+        return (string) rand(999999,9999999);
+    }
+
+    /**
+     * Query with the given tracking_code.
+     * 
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @param  string $code 
+     * @return \Illuminate\Database\Eloquent\Builder       
+     */
+    public function scopeTracking($query, $code)
+    { 
+        return $query->whereIn($this->getQualifiedTrackingCodeColumn(), (array) $code);
+    }
+
+    /**
+     * Query where the tracking_code is not null.
+     * 
+     * @param  \Illuminate\Database\Eloquent\Builder $query 
+     * @return \Illuminate\Database\Eloquent\Builder        
+     */
+    public function scopeTrackable($query)
+    {
+        return $query->whereNotNull($this->getQualifiedTrackingCodeColumn());
+    }
+
+    /**
+     * Get the value of the "tracking_code" column.
+     *
+     * @return string
+     */
+    public function trackingCode()
+    {
+        return $this->getTrackingCode();
+    }
+
+    /**
+     * Get the value of the "tracking_code" column.
+     *
+     * @return string
+     */
+    public function getTrackingCode()
+    {
+        return $this->getAttribute($this->getTrackingCodeColumn());
+    }
+
+    /**
+     * Get the name of the "tracking_code" column.
+     *
+     * @return string
+     */
+    public function getTrackingCodeColumn()
+    {
+        return defined('static::TRACKING_CODE') ? static::TRACKING_CODE : 'tracking_code';
+    }
+
+    /**
+     * Get the fully qualified "tracking_code" column.
+     *
+     * @return string
+     */
+    public function getQualifiedTrackingCodeColumn()
+    {
+        return $this->qualifyColumn($this->getTrackingCodeColumn());
+    }  
+}
---
+++ src/Concerns/QueriesWithResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Armincms\Contract\Concerns;
+
+use Armincms\Contract\Casts\Url;
+ 
+trait QueriesWithResource  
+{    
+    /**
+     * Handles booting model.
+     * 
+     * @return void
+     */
+    public static function bootQueriesWithResource()
+    {
+        static::addGlobalScope(function($query) {
+            return $query->when(static::resources(), function($query) {
+                return $query->resources(static::resources());
+            });
+        });
+    } 
+
+    /**
+     * Query with the given resource.
+     * 
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @param  string $code 
+     * @return \Illuminate\Database\Eloquent\Builder       
+     */
+    public function scopeResources($query, $code)
+    { 
+        return $query->whereIn($this->getQualifiedResourceColumn(), (array) $code);
+    }
+
+    /**
+     * Get valid resources to query.
+     * 
+     * @return array
+     */
+    public static function resources()
+    {
+        return [];
+    }
+
+    /**
+     * Get the name of the "resource" column.
+     *
+     * @return string
+     */
+    public function getResourceColumn()
+    {
+        return defined('static::RESOURCE') ? static::RESOURCE : 'resource';
+    }
+
+    /**
+     * Get the fully qualified "resource" column.
+     *
+     * @return string
+     */
+    public function getQualifiedResourceColumn()
+    {
+        return $this->qualifyColumn($this->getResourceColumn());
+    }  
+}
