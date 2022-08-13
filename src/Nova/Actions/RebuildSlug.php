1a0d5df9c579b3be42244c024a830ba7dbad2191
Ismail Zare <zarehesmaiel@gmail.com>
2022-08-13 14:23:34 +0430

    add action to regenerate URL and Slug

---
+++ src/Nova/Actions/RebuildSlug.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Armincms\Contract\Nova\Actions;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Fields\Boolean;
+
+class RebuildSlug extends Action
+{
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  \Laravel\Nova\Fields\ActionFields  $fields
+     * @param  \Illuminate\Support\Collection  $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        $models->each->forceFill(['slug' => null])->each->save();
+
+        if ($fields->url) {
+            $models->each->fillUri()->each->save();
+        }
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return [
+            Boolean::make(__('Include url'), 'url')
+                ->required()
+                ->rules('required')
+                ->default(false),
+        ];
+    }
+}
